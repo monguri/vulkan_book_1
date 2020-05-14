c3553e65e934b39543e8d0fbef6a7dbd066f0403
monguri <mongry@gmail.com>
2020-05-14 21:43:38 +0900

    ウィンドウを出すところまで実装

---
+++ commonSutraCopy/vkappbase.cpp
@@ -0,0 +1,22 @@
+#include "vkappbase.h"
+
+VulkanAppBase::VulkanAppBase()
+{
+}
+
+void VulkanAppBase::intialize(GLFWwindow* window, const char* appName)
+{
+}
+
+void VulkanAppBase::terminate()
+{
+}
+
+void VulkanAppBase::prepare()
+{
+}
+
+void VulkanAppBase::render()
+{
+}
+
--- commonSutraCopy/vkappbase.h
+++ commonSutraCopy/vkappbase.h
@@ -1,1 +1,23 @@
 #pragma once
+#define WIN32_LEAD_AND_MEAN
+#include <windows.h>
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <vulkan/vk_layer.h>
+#include <vulkan/vulkan_win32.h>
+
+class VulkanAppBase
+{
+public:
+	VulkanAppBase();
+	virtual ~VulkanAppBase() {}
+	void intialize(GLFWwindow* window, const char* appName);
+	void terminate();
+	virtual void prepare();
+	virtual void render();
+};
+
