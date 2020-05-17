f3b2dfbdfb79a3e6b79ca964a174a0b8cdb571b2
monguri <mongry@gmail.com>
2020-05-17 17:51:20 +0900

    頂点バッファ、インデックスバッファ作成

--- 02_SimpleTriangle_SutraCopy/TriangleApp.cpp
+++ 02_SimpleTriangle_SutraCopy/TriangleApp.cpp
@@ -1,7 +1,30 @@
 #include "TriangleApp.h"
 
+using namespace glm;
+
 void TriangleApp::prepare()
 {
+	struct Vertex
+	{
+		glm::vec3 pos;
+		glm::vec3 color;
+	};
+
+	const vec3 red(1.0f, 0.0f, 0.0f);
+	const vec3 green(0.0f, 1.0f, 0.0f);
+	const vec3 blue(0.0f, 0.0f, 1.0f);
+
+	Vertex vertices[] = {
+		{vec3(-1.0f, 0.0f, 0.0f), red}, // ç∂â∫
+		{vec3(1.0f, 0.0f, 0.0f), green}, // âEâ∫
+		{vec3(0.0f, 1.0f, 0.0f), blue}, // ê^ÇÒíÜè„
+	};
+
+	uint32_t indices[] = {0, 1, 2};
+
+	m_vertexBuffer = createBuffer(sizeof(vertices), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+	m_indexBuffer = createBuffer(sizeof(indices), VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
+
 }
 
 void TriangleApp::cleanup()
@@ -12,3 +35,29 @@ void TriangleApp::makeCommand(VkCommandBuffer command)
 {
 }
 
+TriangleApp::BufferObject TriangleApp::createBuffer(uint32_t size, VkBufferUsageFlags usage)
+{
+	BufferObject obj;
+
+	VkBufferCreateInfo ci{};
+	ci.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	ci.usage = usage;
+	ci.size = size;
+	VkResult result = vkCreateBuffer(m_device, &ci, nullptr, &obj.buffer);
+	checkResult(result);
+
+	VkMemoryRequirements reqs;
+	vkGetBufferMemoryRequirements(m_device, obj.buffer, &reqs);
+	VkMemoryAllocateInfo info{};
+	info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	info.allocationSize = reqs.size;
+	VkMemoryPropertyFlags flags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+	info.memoryTypeIndex = getMemoryTypeIndex(reqs.memoryTypeBits, flags);
+	result = vkAllocateMemory(m_device, &info, nullptr, &obj.memory);
+	checkResult(result);
+
+	result = vkBindBufferMemory(m_device, obj.buffer, obj.memory, 0);
+	checkResult(result);
+
+	return obj;
+}
--- 02_SimpleTriangle_SutraCopy/TriangleApp.h
+++ 02_SimpleTriangle_SutraCopy/TriangleApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../commonSutraCopy/vkappbase.h"
+#include "glm/glm.hpp"
 
 class TriangleApp : public VulkanAppBase
 {
@@ -8,5 +9,17 @@ protected:
 	virtual void prepare() override;
 	virtual void cleanup() override;
 	virtual void makeCommand(VkCommandBuffer command) override;
+
+private:
+	struct BufferObject
+	{
+		VkBuffer buffer;
+		VkDeviceMemory memory;
+	};
+
+	BufferObject createBuffer(uint32_t size, VkBufferUsageFlags usage);
+
+	BufferObject m_vertexBuffer;
+	BufferObject m_indexBuffer;
 };
 
