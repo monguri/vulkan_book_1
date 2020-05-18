f9e836bab25ee843252335c2f7863f15079c1a4e
monguri <mongry@gmail.com>
2020-05-18 21:59:36 +0900

    02の写経用プロジェクトからファイルをコピーしてきた

---
+++ 03_TexturedCube_SutraCopy/TriangleApp.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../commonSutraCopy/vkappbase.h"
+#include "glm/glm.hpp"
+
+class TriangleApp : public VulkanAppBase
+{
+protected:
+	virtual void prepare() override;
+	virtual void cleanup() override;
+	virtual void makeCommand(VkCommandBuffer command) override;
+
+private:
+	struct BufferObject
+	{
+		VkBuffer buffer;
+		VkDeviceMemory memory;
+	};
+
+	BufferObject createBuffer(uint32_t size, VkBufferUsageFlags usage);
+	VkPipelineShaderStageCreateInfo loadShaderModule(const char* fileName, VkShaderStageFlagBits stage);
+
+	BufferObject m_vertexBuffer;
+	BufferObject m_indexBuffer;
+
+	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+	VkPipeline m_pipeline = VK_NULL_HANDLE;
+	uint32_t m_indexCount = 0;
+};
+
